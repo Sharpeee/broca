5da60ea139739b75d84da63a1821d19fb7cfed30
Drew DeVault <sir@cmpwn.com>
2018-02-03 23:49:36 -0500

    Remove reaped connections from pool

--- broca/reaper.py
+++ broca/reaper.py
@@ -7,8 +7,9 @@ async def reaper():
     while True:
         await asyncio.sleep(30)
         cutoff = datetime.utcnow() - timedelta(minutes=5)
-        for uri in synapse_pool:
+        for uri in list(synapse_pool.keys()):
             conn = synapse_pool[uri]
             if conn.last_update < cutoff:
                 print(f"Reaping {conn.uuid}")
                 conn.future.cancel()
+                del synapse_pool[uri]
